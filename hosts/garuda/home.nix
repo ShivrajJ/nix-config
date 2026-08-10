bc0866bce722cba8260a659e9000452cbee8eb7e
ShivrajJ <shivraj.jodha@outlook.com>
2026-08-11 02:14:12 +0530

    Enable automatic backupFileExtension in Garuda home.nix

--- hosts/garuda/home.nix
+++ hosts/garuda/home.nix
@@ -5,6 +5,9 @@
   home.homeDirectory = "/home/shivraj";
   home.stateVersion = "24.05";
 
+  # Automatically back up any colliding files during switch (no -b flag needed!)
+  home.backupFileExtension = "backup";
+
   imports = [
     ../../modules/common.nix
     ../../modules/claude.nix
